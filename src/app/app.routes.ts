f419c4410c164d2e35abc8e8c827bf364c4ab3af
Moksh Chadha <mokshchadha@Mokshs-MacBook-Pro.local>
2025-12-15 13:19:49 +0530

    routing update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 import { CommitmentCollationComponent } from './commitment-collation/commitment-collation.component';
 
 export const routes: Routes = [
-    { path: 'commitment-collation', component: CommitmentCollationComponent },
-    { path: '', redirectTo: 'commitment-collation', pathMatch: 'full' }
+    { path: '', component: CommitmentCollationComponent }
 ];
